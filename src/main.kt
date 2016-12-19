37cc288cb70d095874edc3c81d1f080a1339c3ae
just nobody <kingdaro@gmail.com>
2016-12-18 23:03:46 -0500

    Initial commit

---
+++ src/main.kt
@@ -0,0 +1,30 @@
+import java.lang.Math.*
+
+fun main(args: Array<String>) {
+  println("Guess the number (1-100)")
+  val number = ceil(random() * 100).toInt()
+  val guesses = tryGuess(number, 0)
+  println("You win!")
+  println("Took $guesses guesses")
+}
+
+fun tryGuess(number: Int, guesses: Int): Int {
+  val guess = readLine()?.toInt()
+  return if (guess != null) {
+    if (guess < number) {
+      println("Too low")
+      tryGuess(number, guesses + 1)
+    }
+    else if (guess > number) {
+      println("Too high")
+      tryGuess(number, guesses + 1)
+    }
+    else {
+      guesses
+    }
+  }
+  else {
+    println("That's not a number.")
+    tryGuess(number, guesses)
+  }
+}
